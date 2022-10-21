03a9b4a7a992c299ea9f89589d405b98b3182357
Christian <112461646+christianlaurente@users.noreply.github.com>
2022-10-21 08:48:24 +0800

    add google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GTM_ID = "G-R7PK4R35RY";
+
 export default function Document() {
   return (
     <Html>
@@ -17,6 +19,16 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}');
+          `,
+          }}
         />
       </Head>
       <body className="bg-[url('/lined-paper.jpg')]">
