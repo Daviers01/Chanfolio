5aa96967583c5aac1813777180585c34b7cb18be
Daviers01 <daviers01@gmail.com>
2023-05-26 23:43:01 +0800

    add new projects

--- sections/About.tsx
+++ sections/About.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div className="flex flex-col lg:flex-row gap-20 text-xl leading-relaxed tracking-normal font-medium">
       <div className="flex-1">
         <p className="mb-5">
-          I&#39;m a full time developer with over 5 years of professional
+          I&#39;m a full time developer with almost 7 years of professional
           experience. I specialise in front-end development, but over the years
           I also learned a lot in back-end stuffs.
         </p>
@@ -15,8 +15,8 @@ const About = () => {
           <span className="text-[#3c4540] font-bold">Node</span>.
           <span>
             I am now interested in learning{" "}
-            <span className="text-[#bc4fc0] font-bold">Qwik</span>,cause I think
-            it would be popular in the near future.
+            <span className="text-[#bc4fc0] font-bold">Qwik</span>, cause I
+            think it would be popular in the near future.
           </span>
         </p>
       </div>
@@ -24,7 +24,7 @@ const About = () => {
         <p className="mb-5">
           I am currently working at{" "}
           <span className="text-[#6a2c55] font-bold">Accenture Inc.</span> as
-          Advanced App Engineering Specialist.
+          Software Engineering Specialist.
         </p>
         <p className="mb-5">
           I am certified Cloud Practitioner in AWS and Microsoft Azure and
